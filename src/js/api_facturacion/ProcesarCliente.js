b3e849185b8deaca3651574a1f950c5079a2916c
Jimmy Ortega <fabricioortega2011@gmail.com>
2023-02-22 19:47:10 -0500

    Taller33

---
+++ src/js/api_facturacion/ProcesarCliente.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+//metodo fachadas
+
+
+
+
+//metodos de consumo CRUD
+
+
+const obtenerClienteporCedula=(cedula)=>{
+
+}
+
+const insertar=(body)=>{
+
+}
+
+const actualizar=(cedula, body)=>{
+
+}
